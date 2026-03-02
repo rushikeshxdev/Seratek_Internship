145df5e39a361a20b062b61b45c60abf78e158eb
Rushikesh Randive <rushirandive09@gmail.com>
2026-03-02 11:18:56 +0530

    Add auto-updating README with live stats

--- Day-1/Simple-CRUD-App/index.js
+++ Day-1/Simple-CRUD-App/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.send('Hello, Node API Server Running!');
 });
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
-});
+}); 
